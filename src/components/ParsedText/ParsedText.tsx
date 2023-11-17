037b5296cc96173953328f240ff95744a176c76f
Pedro Borges <ppmbjr@gmail.com>
2023-11-17 02:35:41 -0300

    chore: added more explanation about how to use the ParsedText component

--- src/components/ParsedText/ParsedText.tsx
+++ src/components/ParsedText/ParsedText.tsx
@@ -7,7 +7,12 @@ export interface ParsedTextProps extends TextProps {
   patterns?: PatternShape[];
   childrenProps?: TextProps;
 }
-
+/**
+ * @param patterns - An array of PatternShape objects. Each object represents a pattern to be parsed within the main string.
+ * @param style - The style applied to the entire string.
+ * @param testID - The value applied to the 'span' element wrapping the entire text and all substrings.
+ * @param attributes - It's possible to pass any attribute from the span element.
+ */
 export const ParsedText: React.FC<ParsedTextProps> = ({
   patterns = [],
   childrenProps = [],
